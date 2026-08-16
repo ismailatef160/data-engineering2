ff9006b6083c95f508a2da58cae1170d446e8220
Ismail <ismailatef2005@gmail.com>
2026-08-16 13:45:14 +0000

    sdfsd

--- pipeline/pipeline.py
+++ pipeline/pipeline.py
@@ -1,5 +1,6 @@
 import sys
 import pandas as pd
+import os
 
 print('arguments',sys.argv)
 
@@ -7,6 +8,7 @@ month = int(sys.argv[1])
 df = pd.DataFrame({"A": [1, 2], "B": [3, 4]})
 df['month'] = month
 print(df.head())
+os.makedirs('output', exist_ok=True)
 df.to_parquet(f"output/month={month}.parquet")
 
 print(f'hello pipeline,month={month}')
